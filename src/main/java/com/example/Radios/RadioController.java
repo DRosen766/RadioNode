7b032b5b55cfa712e0bc01f7b12f59c5fedb6db3
Daniel Rosen <daniel20@1311-1072-39.ad.hume.vt.edu>
2023-11-11 15:41:55 -0500

    init

---
+++ src/main/java/com/example/Radios/RadioController.java
@@ -0,0 +1,20 @@
+package com.example.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RadioController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/receiveiq")
+	public void receiveIQ(@RequestParam(value = "name", defaultValue = "World") String name) {
+        System.out.print("Hello world!!");
+		// return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
