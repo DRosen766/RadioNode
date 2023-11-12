4ba747c2afa8eb29393e0841b3d526bac82134e7
Danny Rosen <daniel20@vt.edu>
2023-11-12 14:57:48 -0500

    functional sending iq array

--- RadioClient/radioclient.py
+++ RadioClient/radioclient.py
@@ -3,8 +3,9 @@ import urllib.parse
 import json
 import os
 ip_addr = "localhost"
-# params = urllib.parse.urlencode({"@name" : "sentname"})
-# foo = {'name': 'Hello HTTP #1 **cool**, and #1!'}
-# json_data = json.dumps(foo)
+params = urllib.parse.urlencode({"word" : [1,2,3]})
+print(params)
+foo = {'word': 'Hello HTTP #1 **cool**, and #1!'}
+json_data = json.dumps(foo)
 radioConnection = client.HTTPConnection("{}:5000".format(ip_addr))
-radioConnection.request("GET", "/")
+radioConnection.request("PUT", "/?{}".format(params), body="asdf")
--- RadioServer/radio_server.py
+++ RadioServer/radio_server.py
@@ -1,8 +1,24 @@
-from flask import Flask
-
+from flask import Flask, request
+from werkzeug.utils import secure_filename
+import boto3
+import array
+import numpy as np
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def hello_world():
-    print("success!")
-    return "<p>Hello, World!</p>"
+iq_data_sample = []
+
+@app.put("/send_iq")
+def append_iq():
+    k = int(request.args.get("k"))
+
+    # extract iq data from request
+    iq_data = request.get_data()
+    iq_data = np.array(array.array('f', iq_data))
+
+    # move iq_data to file
+    iqdata_file_name = 'server_data/iqdata/example_' + str(k+1) + '.dat'
+    iq_data.tofile(iqdata_file_name)
+
+    # upload file to s3 bucket
+    boto3.resource('s3').Bucket('radio-bucket-766318').upload_file(iqdata_file_name, iqdata_file_name)
+    return "created {}".format(iqdata_file_name)
