f0adde260b63932d8e51cc6b3313d3928290b8b1
Danny Rosen <daniel20@vt.edu>
2023-12-17 16:16:31 -0500

    add mqtt connection func, new script starteed for writing iq data to iot

--- RadioClient/iot_client.py
+++ RadioClient/iot_client.py
@@ -4,85 +4,65 @@
 from awscrt import mqtt, http
 from awsiot import mqtt_connection_builder
 from uuid import uuid4
-import sys
-import threading
 import time
 import json
 import os
-import requests
+import numpy as np
 if __name__ == '__main__':
-    # parse and load command-line parameter values
-
 # create and format values for HTTPS request
-    publish_url = 'https://' + os.environ["AWS_IOT_ENDPOINT"] + ':8443/topics/' + os.environ["TOPIC"] + '?qos=1'
-    publish_msg = "hello world message"
+    topic = "iq_data"
+    mqtt_connection = mqtt_connection_builder.mtls_from_path(
+        endpoint=os.environ["AWS_IOT_ENDPOINT"],
+        cert_filepath=os.environ["AWS_IOT_CERT"],
+        pri_key_filepath=os.environ["AWS_IOT_KEY"],
+        ca_filepath=os.environ["AWS_IOT_CA_FILE"],
+        client_id="danny_rosen")
+    connect_future = mqtt_connection.connect()
 
-    # make request
-    publish = requests.request('POST',
-            publish_url,
-            data=publish_msg,
-            cert=[os.environ["AWS_IOT_CERT"], os.environ["AWS_IOT_KEY"]])
-    # # Create the proxy options if the data is present in cmdData
-    # # proxy_options = None
-    # # if cmdData.input_proxy_host is not None and cmdData.input_proxy_port != 0:
-    # #     proxy_options = http.HttpProxyOptions(
-    # #         host_name=cmdData.input_proxy_host,
-    # #         port=cmdData.input_proxy_port)
+    # Future.result() waits until a result is available
+    connect_future.result()
+    print("Connected!")
 
-    # # Create a MQTT connection from the command line data
-    # print(os.environ["AWS_IOT_ENDPOINT"], os.environ["AWS_IOT_KEY"])
-    # mqtt_connection = mqtt_connection_builder.mtls_from_path(
-    #     endpoint=os.environ["AWS_IOT_ENDPOINT"],
-    #     cert_filepath=os.environ["AWS_IOT_CERT"],
-    #     pri_key_filepath=os.environ["AWS_IOT_KEY"],
-    #     ca_filepath=os.environ["AWS_IOT_CA_FILE"],
-    #     client_id="test-" + str(uuid4()))
-    # connect_future = mqtt_connection.connect()
+    message_topic = topic
+    message_string = "message!"
 
-    # # Future.result() waits until a result is available
-    # connect_future.result()
-    # print("Connected!")
+    # Subscribe
+    print("Subscribing to topic '{}'...".format(message_topic))
+    subscribe_future, packet_id = mqtt_connection.subscribe(
+        topic=message_topic,
+        qos=mqtt.QoS.AT_LEAST_ONCE)
 
-    # message_topic = os.environ["TOPIC"]
-    # message_string = "message!"
+    subscribe_result = subscribe_future.result()
+    print("Subscribed with {}".format(str(subscribe_result['qos'])))
 
-    # # Subscribe
-    # print("Subscribing to topic '{}'...".format(message_topic))
-    # subscribe_future, packet_id = mqtt_connection.subscribe(
-    #     topic=message_topic,
-    #     qos=mqtt.QoS.AT_LEAST_ONCE)
+    # Publish message to server desired number of times.
+    # This step is skipped if message is blank.
+    # This step loops forever if count was set to 0.
+    # if message_string:
+    #     if message_count == 0:
+    #         print("Sending messages until program killed")
+    #     else:
+    #         print("Sending {} message(s)".format(message_count))
 
-    # subscribe_result = subscribe_future.result()
-    # print("Subscribed with {}".format(str(subscribe_result['qos'])))
+    message = f"iq_data: [{np.ones(5)}]"
+    print("Publishing message to topic '{}': {}".format(message_topic, message))
+    message_json = json.dumps(message)
+    mqtt_connection.publish(
+        topic=message_topic,
+        payload=message_json,
+        qos=mqtt.QoS.AT_LEAST_ONCE)
+    time.sleep(1)
 
-    # # Publish message to server desired number of times.
-    # # This step is skipped if message is blank.
-    # # This step loops forever if count was set to 0.
-    # # if message_string:
-    # #     if message_count == 0:
-    # #         print("Sending messages until program killed")
-    # #     else:
-    # #         print("Sending {} message(s)".format(message_count))
+    # # Wait for all messages to be received.
+    # # This waits forever if count was set to 0.
+    # if message_count != 0 and not received_all_event.is_set():
+    #     print("Waiting for all messages to be received...")
 
-    # message = "message: {}".format(message_string)
-    # print("Publishing message to topic '{}': {}".format(message_topic, message))
-    # message_json = json.dumps(message)
-    # mqtt_connection.publish(
-    #     topic=message_topic,
-    #     payload=message_json,
-    #     qos=mqtt.QoS.AT_LEAST_ONCE)
-    # time.sleep(1)
+    # received_all_event.wait()
+    # print("{} message(s) received.".format(received_count))
 
-    # # # Wait for all messages to be received.
-    # # # This waits forever if count was set to 0.
-    # # if message_count != 0 and not received_all_event.is_set():
-    # #     print("Waiting for all messages to be received...")
-
-    # # received_all_event.wait()
-    # # print("{} message(s) received.".format(received_count))
-
-    # # # Disconnect
-    # # print("Disconnecting...")
-    # # disconnect_future = mqtt_connection.disconnect()
-    # # disconnect_future.result()
-    # # print("Disconnected!")
+    # # Disconnect
+    # print("Disconnecting...")
+    # disconnect_future = mqtt_connection.disconnect()
+    # disconnect_future.result()
+    # print("Disconnected!")
---
+++ RadioClient/send_iq_data_s3.py
@@ -0,0 +1,117 @@
+# Generate and Send IQ Data to Radio Server
+
+from http import client
+import requests
+import csv
+import time
+import numpy as np
+import matplotlib.pyplot as pyplot
+import torch
+from torch import Tensor
+from tqdm import tqdm
+import pandas as pd
+
+
+# Datagen Imports
+from cloudd_rf.iqdata_gen import iqdata_gen
+from cloudd_rf.metadata_gen import metadata_gen
+from cloudd_rf.imagedata_gen import imagedata_gen
+# Dataset Parameters
+rand_seed = 10                                            # Seed for the random number generator for repeatability (note: script must use all of the same generation parameter bounds and values).
+num_training_examples = 50                                     # Number of different radio frequency spectrum examples to be created for the dataset.
+num_testing_examples = 10                                      # Number of different radio frequency spectrum examples to be created for the dataset.
+max_sigs = 1
+# Spectrum Parameters
+obs_int = 1024                                              # Observation length of the spectrum for each example.
+max_trials = 10000                                          # How many tries the generator will attempt to fit the maximum number of signals in the spectrum (note: if allow_collision=True, this parameter doesn't do anything).
+bandwidth_bounds = [0.25, 0.5]                              # Bandwidth min and max of any created signal with possible range (0.0, 1.0).
+cent_freq_bounds = [-0.25, 0.25]                            # Center Frequency min and max of any created signal with possible range [-0.5, 0.5].
+start_bounds = [0, int(0.25*obs_int)]                       # Starting sample min and max of any created signal with possible range [0, obs_int].
+duration_bounds = [int(0.5*obs_int), int(0.75*obs_int)]     # Duration (in samples) min and max of any created signal with possible range (0, obs_int].
+snr_bounds = [5, 20]                                        # SNR (in dB) min and max of any created signal.
+sig_types = [['2-ASK',  ['ask', 2]],                        # Potential signal types of any created signal.
+             ['4-ASK',  ['ask', 4]],
+             ['8-ASK',  ['ask', 8]],
+             ['BPSK',   ['psk', 2]], 
+             ['QPSK',   ['psk', 4]],
+             ['8-PSK',  ['psk', 8]],
+             ['16-QAM', ['qam', 16]],
+             ['64-QAM', ['qam', 64]],
+             ['Constant Tone', ['constant']],
+             ['P-FMCW', ['p_fmcw']],
+             ['N-FMCW', ['n_fmcw']]]
+
+allow_collisions = False                                    # True: Signals can be overlapped in time and/or frequency. False: No overlap in signals but may not generate max_sigs.
+
+# Image Parameters
+image_width = 1000                                          # Image width (in pixels).
+image_height = 500                                          # Image height (in pixels).
+fft_size = 256                                              # FFT size used to generate the spectrogram image.
+overlap = 255                                               # FFT overlap used to generate the spectrogram image.
+
+# Initialize Metadata File
+metadata_file_name = 'metadata.csv'
+# fid = open(metadata_file_name, 'w', encoding='UTF8', newline='')
+# writer = csv.writer(fid)
+# header = ['file_name', 'Center Frequency', 'Bandwidth', 'Start Time (samples)', 'Stop Time (samples)', 'SNR', 'Signal Type']
+# writer.writerow(header)
+
+# Initalize Generators
+rng = np.random.default_rng(rand_seed)
+
+meta_gen = metadata_gen(obs_int, rng)
+iq_gen = iqdata_gen(obs_int, rng)
+im_gen = imagedata_gen(image_width, image_height, fft_size, overlap)
+label_map = {"['2-ASK', ['ask', 2]]" : 0,
+             "['4-ASK', ['ask', 4]]" : 1,
+             "['8-ASK', ['ask', 8]]" : 2,
+             "['BPSK', ['psk', 2]]" : 3, 
+             "['QPSK', ['psk', 4]]" : 4,
+             "['8-PSK', ['psk', 8]]" : 5,
+             "['16-QAM', ['qam', 16]]" : 6,
+             "['64-QAM', ['qam', 64]]" : 7,
+             "['Constant Tone', ['constant']]" : 8,
+             "['P-FMCW', ['p_fmcw']]" : 9,
+             "['N-FMCW', ['n_fmcw']]" : 10}
+
+# Init http
+# ip_addr = "18.191.171.241:5000"
+ip_addr = "http://127.0.0.1:5000"
+metadata_file_name = 'metadata.csv'
+fid = open(metadata_file_name, 'w', encoding='UTF8', newline='')
+writer = csv.writer(fid)
+
+
+
+training_examples = []
+training_sig_types = []
+training_snr = []
+testing_examples = []
+testing_sig_types = []
+testing_snr = []
+
+# Create Dataset
+radioConnection = requests.Session()
+for k in tqdm(range(num_training_examples + num_testing_examples)):
+    start_time = time.time()
+    burst_metadata = meta_gen.gen_metadata(max_sigs, max_trials, bandwidth_bounds, cent_freq_bounds, start_bounds, duration_bounds, snr_bounds, sig_types, allow_collisions)
+    iq_data, burst_metadata = iq_gen.gen_iq(burst_metadata)
+    iq_data_clone = np.copy(iq_data)
+    iq_data = np.concatenate((np.real(iq_data),np.imag(iq_data_clone)))
+    iqdata_file_name = 'server_data/iqdata/example_' + str(k) + '.dat'
+    radioConnection.put("{}{}".format(ip_addr, "/send_iq"), params={"k":k, "iqdata_file_name":iqdata_file_name}, data=bytearray(iq_data))
+
+    for metadata in burst_metadata:
+        # extract and send params
+        params = {"metadata_file_name" : metadata_file_name,
+                "iqdata_file_name":iqdata_file_name, 
+                  "cent_freq" : metadata.cent_freq, 
+                  "bandwidth" : metadata.bandwidth, 
+                  "snr" : metadata.snr, 
+                  "sig_type" : str(metadata.sig_type)}
+        radioConnection.put("{}{}".format(ip_addr, "/send_metadata"), params= params)
+
+# after all metadata has been created, load it to s3 with endpoint
+radioConnection.post("{}{}".format(ip_addr, "/upload_metadata"), params= {"metadata_file_name":metadata_file_name})
+
+
