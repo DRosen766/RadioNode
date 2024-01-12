4cbf05b06cad662abeee9f0d47815c71369f82c1
Danny Rosen <daniel20@vt.edu>
2024-01-12 11:38:47 -0500

    delete extraneous files

--- Model/driver.py
+++
@@ -1,8 +0,0 @@
-from sagemaker.estimator import Estimator
-
-estimator = Estimator(image_name="custom-training-container",
-                      role="SageMakerRole",
-                      train_instance_count=1,
-                      train_instance_type="local")
-
-estimator.fit()
--- Model/estimator.py
+++
@@ -1,7 +0,0 @@
-from sagemaker.estimator import Estimator
-import boto3
-# client = boto3.client('sagemaker').role
-
-estimator = Estimator(image_uri="model_container", entry_point="sandbox.py", role="AWSServiceRoleForAmazonSageMakerNotebooks", instance_type="local", instance_count=1)
-
-estimator.fit()
