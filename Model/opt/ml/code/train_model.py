b575c8dedc75fdab045d38b5b75b836422ee8c9a
Danny Rosen <daniel20@vt.edu>
2024-01-13 09:14:51 -0500

    added default model_fn

--- Model/opt/ml/code/train_model.py
+++ Model/opt/ml/code/train_model.py
@@ -28,100 +28,108 @@ transform = transforms.Compose(
 transforms.Normalize((0.5,), (0.5,))])
 
 
+def model_fn(model_dir):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = torch.nn.DataParallel(Net())
+    with open(os.path.join(model_dir, "model.pth"), "rb") as f:
+        model.load_state_dict(torch.load(f))
+    return model.to(device)
+
+
+if __name__ == "__main__":
+    label_map = {"['2-ASK', ['ask', 2]]" : 0,
+                "['4-ASK', ['ask', 4]]" : 1,
+                "['8-ASK', ['ask', 8]]" : 2,
+                "['BPSK', ['psk', 2]]" : 3, 
+                "['QPSK', ['psk', 4]]" : 4,
+                "['8-PSK', ['psk', 8]]" : 5,
+                "['16-QAM', ['qam', 16]]" : 6,
+                "['64-QAM', ['qam', 64]]" : 7,
+                "['Constant Tone', ['constant']]" : 8,
+                "['P-FMCW', ['p_fmcw']]" : 9,
+                "['N-FMCW', ['n_fmcw']]" : 10}
+
+    # access /opt/ml/directory
+    true_path = lambda path : realpath(join("..", normpath(path)))
+
+    # connect to bucket
+    bucket = boto3.resource("s3").Bucket(os.environ["SM_TRAINING_DATA_BUCKET"])
+    bucket.download_file("label_map.json", true_path("label_map.json"))
+    label_map = json.load(open(true_path("label_map.json")))
+    bucket.download_file("train/train_metadata.csv", true_path("train_data/train_metadata.csv"))
+    metadata_file = open(true_path("train_data/train_metadata.csv"))
+    reader = csv.reader(metadata_file)
+    reader = list(reader)
 
 
 
-label_map = {"['2-ASK', ['ask', 2]]" : 0,
-             "['4-ASK', ['ask', 4]]" : 1,
-             "['8-ASK', ['ask', 8]]" : 2,
-             "['BPSK', ['psk', 2]]" : 3, 
-             "['QPSK', ['psk', 4]]" : 4,
-             "['8-PSK', ['psk', 8]]" : 5,
-             "['16-QAM', ['qam', 16]]" : 6,
-             "['64-QAM', ['qam', 64]]" : 7,
-             "['Constant Tone', ['constant']]" : 8,
-             "['P-FMCW', ['p_fmcw']]" : 9,
-             "['N-FMCW', ['n_fmcw']]" : 10}
-
-# access /opt/ml/directory
-true_path = lambda path : realpath(join("..", normpath(path)))
-
-# connect to bucket
-bucket = boto3.resource("s3").Bucket(os.environ["SM_TRAINING_DATA_BUCKET"])
-bucket.download_file("label_map.json", true_path("label_map.json"))
-label_map = json.load(open(true_path("label_map.json")))
-bucket.download_file("train/train_metadata.csv", true_path("train_data/train_metadata.csv"))
-metadata_file = open(true_path("train_data/train_metadata.csv"))
-reader = csv.reader(metadata_file)
-reader = list(reader)
-
-# datasets
-train_num = 100
-training_examples = []
-training_labels = []
+    # datasets
+    train_num = 100
+    training_examples = []
+    training_labels = []
 
 
-def load_data(num_threads, thread_num, test_data=True, reader=reader):
-    reader = reader[int((train_num / num_threads) * thread_num) : int((train_num / num_threads) * thread_num + (train_num / num_threads) )]
-    for line in tqdm(reader):
-        data_file_name, snr, label = line[0], line[-2], line[-1]
-        # download from file and load to array
-        bucket.download_file(f"train/iqdata/{data_file_name}", true_path(f"train_data/iqdata/{data_file_name}"))
-        iq_data = np.fromfile(true_path(f"train_data/iqdata/{data_file_name}"))
-        # stack array
-        iq_data = np.vstack(([[iq_data[:1024]]], [[iq_data[1024:]]]))
-        # add to dataset
-        training_examples.append(iq_data)
-        training_labels.append(label_map[label])
+    def load_data(num_threads, thread_num, test_data=True, reader=reader):
+        reader = reader[int((train_num / num_threads) * thread_num) : int((train_num / num_threads) * thread_num + (train_num / num_threads) )]
+        for line in tqdm(reader):
+            data_file_name, snr, label = line[0], line[-2], line[-1]
+            # download from file and load to array
+            bucket.download_file(f"train/iqdata/{data_file_name}", true_path(f"train_data/iqdata/{data_file_name}"))
+            iq_data = np.fromfile(true_path(f"train_data/iqdata/{data_file_name}"))
+            # stack array
+            iq_data = np.vstack(([[iq_data[:1024]]], [[iq_data[1024:]]]))
+            # add to dataset
+            training_examples.append(iq_data)
+            training_labels.append(label_map[label])
 
-threads = [Thread(target=load_data, args=[6,i, True if i < 5 else False],name="thread{}".format(i)) for i in range(6)]
+    threads = [Thread(target=load_data, args=[6,i, True if i < 5 else False],name="thread{}".format(i)) for i in range(6)]
 
-for thread_run in threads:
-    thread_run.start()
-for thread_join in threads:
-    thread_join.join()
+    for thread_run in threads:
+        thread_run.start()
+    for thread_join in threads:
+        thread_join.join()
 
-iteration = 0
-while True and iteration < 1:
-    batch_size_train = [64]
-    batch_size_test = [16]
-    learning_rate = [0.001]
-    weight_decay = [1e-3]
-    epochs = [2]
-    
+    iteration = 0
+    while True and iteration < 1:
+        batch_size_train = [64]
+        batch_size_test = [16]
+        learning_rate = [0.001]
+        weight_decay = [1e-3]
+        epochs = [2]
+        
 
-    training_examples = Tensor(np.array(training_examples))
-    training_labels = Tensor(np.array(training_labels))
-    train_ds = TensorDataset(training_examples, training_labels)
-    trainloader = DataLoader(train_ds,batch_size=int(batch_size_train[iteration]),
-    shuffle=True, num_workers=0)
+        training_examples = Tensor(np.array(training_examples))
+        training_labels = Tensor(np.array(training_labels))
+        train_ds = TensorDataset(training_examples, training_labels)
+        trainloader = DataLoader(train_ds,batch_size=int(batch_size_train[iteration]),
+        shuffle=True, num_workers=0)
 
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    device = 'cpu'
-    net = Net()
-    net.to(device)
-    optimizer = optim.Adam(net.parameters(), lr=float(learning_rate[iteration]), weight_decay=float(weight_decay[iteration]))
-    accuracy = 0.0
-    losses = []
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        device = 'cpu'
+        net = Net()
+        net.to(device)
+        optimizer = optim.Adam(net.parameters(), lr=float(learning_rate[iteration]), weight_decay=float(weight_decay[iteration]))
+        accuracy = 0.0
+        losses = []
 
-    for epoch in range(int(epochs[iteration])): # loop over the dataset multiple times
-        print('Epoch-{0} lr: {1}'.format(epoch + 1, optimizer.param_groups[0]['lr']))
-        for batch_idx, (input, target) in tqdm(enumerate(trainloader)):
-            if device == 'cuda':
-                input, target = input.cuda(), target.cuda()
-            optimizer.zero_grad()
-            outputs = net(input, input.shape[0])
-            loss = F.nll_loss(outputs, target.long())
-            loss.backward()
-            optimizer.step()
-            losses.append(loss.item())
-        plt.plot(range(len(losses)), losses)
-        # plt.savefig("output_plot_{}.png".format(iteration))
-        plt.close()
+        for epoch in range(int(epochs[iteration])): # loop over the dataset multiple times
+            print('Epoch-{0} lr: {1}'.format(epoch + 1, optimizer.param_groups[0]['lr']))
+            for batch_idx, (input, target) in tqdm(enumerate(trainloader)):
+                if device == 'cuda':
+                    input, target = input.cuda(), target.cuda()
+                optimizer.zero_grad()
+                outputs = net(input, input.shape[0])
+                loss = F.nll_loss(outputs, target.long())
+                loss.backward()
+                optimizer.step()
+                losses.append(loss.item())
+            plt.plot(range(len(losses)), losses)
+            # plt.savefig("output_plot_{}.png".format(iteration))
+            plt.close()
 
-    torch.save(net.state_dict(), true_path("saved_model"))
+        torch.save(net.state_dict(), true_path("saved_model"))
+
     exit()
-
 
     # iterate over test data
     for inputs, snr, label in tqdm(testloader):
--- Model/tests/test_model_estimator.py
+++ Model/tests/test_model_estimator.py
@@ -5,3 +5,5 @@ s3_output_location=f's3://{test_bucket_name}/saved_model'
 estimator = Estimator(image_uri="model_container", role="AWSServiceRoleForAmazonSageMakerNotebooks", output_path=s3_output_location, instance_type="local", instance_count=1)
 # param defines SM_CHANNEL_TRAINING
 estimator.fit({"training":f"s3://{test_bucket_name}/test/test_metadata.csv"})
+
+predictor = estimator.deploy(initial_instance_count=1, instance_type="local")
